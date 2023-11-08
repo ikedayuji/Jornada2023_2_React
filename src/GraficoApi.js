ab83b63d8c1ce49ec1ee3cb6651c25b881dfc6bb
Eric <ikedayuji.2002@gmail.com>
2023-11-07 21:34:33 -0300

    Jornada v7.3

--- src/GraficoApi.js
+++ src/GraficoApi.js
@@ -11,32 +11,232 @@ const chartStyle = {
 
 const containerStyle = {
   display: 'flex',
-  flexWrap: 'wrap',
+  flexDirection: 'column',
+  alignItems: 'center',
 };
 
-const cardContainerStyle = {
+const rowContainerStyle = {
   display: 'flex',
 };
 
 const cardStyle = {
-  ...chartStyle,  // Mantém as propriedades do cartão
-  float: 'left', // Adiciona float para alinhar à esquerda
+  ...chartStyle,
 };
 
 function GraficoMinutoPeso() {
-  // Componente GraficoMinutoPeso existente, mantido igual
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current);
+
+    const option = {
+      title: {
+        text: 'Sensores de Temperatura',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Peso (kg)'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '00:01', '00:02', '00:03', '00:04', '00:05', '00:06'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Peso (kg)',
+          type: 'line',
+          data: [100, 120, 110, 105, 90, 85, 95],
+        },
+      ],
+    };
+
+    myChart.setOption(option);
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} style={cardStyle} />;
 }
 
 function GraficoMinutoNivel() {
-  // Componente GraficoMinutoNivel existente, mantido igual
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current);
+
+    const option = {
+      title: {
+        text: 'Sensores de Nível',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Nível'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '00:01', '00:02', '00:03', '00:04', '00:05', '00:06'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Nível',
+          type: 'line',
+          data: [50, 60, 55, 58, 52, 62, 59],
+        },
+      ],
+    };
+
+    myChart.setOption(option);
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} style={cardStyle} />;
 }
 
 function GraficoHoraPeso() {
-  // Componente GraficoHoraPeso existente, mantido igual
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current);
+
+    const option = {
+      title: {
+        text: 'Sensores de Fluxo',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Peso (kg)'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Peso (kg)',
+          type: 'line',
+          data: [1200, 1320, 1010, 1340, 900, 2300, 2100],
+        },
+      ],
+    };
+
+    myChart.setOption(option);
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} style={cardStyle} />;
 }
 
 function GraficoHoraNivel() {
-  // Componente GraficoHoraNivel existente, mantido igual
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current);
+
+    const option = {
+      title: {
+        text: 'Sensores de Velocidade',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Nível'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Nível',
+          type: 'line',
+          data: [50, 60, 55, 58, 52, 62, 59],
+        },
+      ],
+    };
+
+    myChart.setOption(option);
+
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+
+  return <div ref={chartRef} style={cardStyle} />;
 }
 
 function GraficoNovo1() {
@@ -47,9 +247,40 @@ function GraficoNovo1() {
 
     const option = {
       title: {
-        text: 'Sensores de Temperatura',
+        text: 'Sensores de Nível',
       },
-      // Adicione configurações específicas para o Gráfico Novo 1
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Dados 1'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '00:01', '00:02', '00:03', '00:04', '00:05', '00:06'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Dados 1',
+          type: 'line',
+          data: [10, 20, 30, 40, 50, 40, 30],
+        },
+      ],
     };
 
     myChart.setOption(option);
@@ -70,101 +301,40 @@ function GraficoNovo2() {
 
     const option = {
       title: {
-        text: 'Peso dos Materiais',
-      },
-      // Adicione configurações específicas para o Gráfico Novo 2
-    };
-
-    myChart.setOption(option);
-
-    return () => {
-      myChart.dispose();
-    };
-  }, []);
-
-  return <div ref={chartRef} style={cardStyle} />;
-}
-
-function GraficoNovo3() {
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const myChart = echarts.init(chartRef.current);
-
-    const option = {
-      title: {
-        text: 'Sensores de Nível',
-      },
-      // Adicione configurações específicas para o Gráfico Novo 3
-    };
-
-    myChart.setOption(option);
-
-    return () => {
-      myChart.dispose();
-    };
-  }, []);
-
-  return <div ref={chartRef} style={cardStyle} />;
-}
-
-function GraficoNovo4() {
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const myChart = echarts.init(chartRef.current);
-
-    const option = {
-      title: {
-        text: 'Sensores de Fluxo',
-      },
-      // Adicione configurações específicas para o Gráfico Novo 4
-    };
-
-    myChart.setOption(option);
-
-    return () => {
-      myChart.dispose();
-    };
-  }, []);
-
-  return <div ref={chartRef} style={cardStyle} />;
-}
-
-function GraficoNovo5() {
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const myChart = echarts.init(chartRef.current);
-
-    const option = {
-      title: {
-        text: 'Sensores de Velocidade',
-      },
-      // Adicione configurações específicas para o Gráfico Novo 5
-    };
-
-    myChart.setOption(option);
-
-    return () => {
-      myChart.dispose();
-    };
-  }, []);
-
-  return <div ref={chartRef} style={cardStyle} />;
-}
-
-function GraficoNovo6() {
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const myChart = echarts.init(chartRef.current);
-
-    const option = {
-      title: {
         text: 'Reservado',
       },
-      // Adicione configurações específicas para o Gráfico Novo 6
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Dados 2'],
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['00:00', '00:01', '00:02', '00:03', '00:04', '00:05', '00:06'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Dados 2',
+          type: 'line',
+          data: [15, 25, 35, 45, 55, 45, 35],
+        },
+      ],
     };
 
     myChart.setOption(option);
@@ -176,20 +346,23 @@ function GraficoNovo6() {
 
   return <div ref={chartRef} style={cardStyle} />;
 }
+
+// ... Código para os gráficos restantes ...
 
 function App() {
   return (
     <div>
       <div style={containerStyle}>
-        <div style={cardContainerStyle}>
+        <div style={rowContainerStyle}>
+          <GraficoMinutoPeso />
+          <GraficoMinutoNivel />
+          <GraficoHoraPeso />
+        </div>
+        <div style={rowContainerStyle}>
+          <GraficoHoraNivel />
           <GraficoNovo1 />
           <GraficoNovo2 />
-          <GraficoNovo5 />
-        </div>
-        <div style={cardContainerStyle}>
-          <GraficoNovo3 />
-          <GraficoNovo4 />
-          <GraficoNovo6 />
+          {/* Adicione os outros gráficos GraficoNovo3, GraficoNovo4, GraficoNovo5, GraficoNovo6 aqui */}
         </div>
       </div>
     </div>
